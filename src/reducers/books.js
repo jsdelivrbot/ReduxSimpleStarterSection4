3570d95135e581a475d5e508209b2034420f674a
João Amaral <jamaral@pasonatquila.com>
2017-05-17 11:51:46 +0900

    Section 4 Completed, added images as surplus

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts', pages: 43, cover: 'https://www.oreilly.co.jp/books/images/picture_large978-4-87311-391-3.jpeg' },
+    { title: '入門 React', pages: 404, cover:'https://www.oreilly.co.jp/books/images/picture_large978-4-87311-719-5.jpeg'},
+    { title: 'Future of the Mind', pages: 500, cover:'https://upload.wikimedia.org/wikipedia/en/4/4c/The_Future_of_the_Mind_bookcover.jpg'},
+    { title: 'Eloquent Javascript', pages: 1, cover:'http://eloquentjavascript.net/img/cover.png'}
+  ]
+}
